cfcc1f0100940538a942730330b726f970db3199
Tiago Ramirez <tiagoramirez.dev@proton.me>
2026-01-27 23:34:45 -0300

    Fixed metrics config

--- src/main/java/com/tiagoramirez/template/config/AppConfig.java
+++ src/main/java/com/tiagoramirez/template/config/AppConfig.java
@@ -1,7 +1,7 @@
 package com.tiagoramirez.template.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.micrometer.metrics.autoconfigure.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
